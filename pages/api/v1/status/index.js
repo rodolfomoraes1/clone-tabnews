cf7930d372b02f55050d77b264f000dcac1bed42
Rodolfo Martins <rodolfomoraes1@gmail.com>
2024-01-13 22:09:10 +0000

    finalização da api `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log("result", result.rows);
-  response.status(200).json({ message: "status working!" });
+  const version = await database.query("SHOW server_version");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const version = "16.0";
+  expect(responseBody.dependencies.database.version).toEqual(version);
+
+  const maxConnections = 100;
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    maxConnections,
+  );
+
+  const openedConnections = 1;
+  expect(responseBody.dependencies.database.opened_connections).toEqual(
+    openedConnections,
+  );
+
+  console.log("response", responseBody);
 });
